3269354185d042ae3da98666de6aa230824ae24c
Fruit-Punch-Samurai <58863987+Fruit-Punch-Samurai@users.noreply.github.com>
2021-02-09 20:59:22 +0100

    Implemented a dummy DrawerLayout
    Signed-off-by: Fruit-Punch-Samurai <58863987+Fruit-Punch-Samurai@users.noreply.github.com>

--- app/src/main/java/tv/bain/bainsocial/fragments/HomeFrag.java
+++ app/src/main/java/tv/bain/bainsocial/fragments/HomeFrag.java
@@ -1,5 +1,6 @@
 package tv.bain.bainsocial.fragments;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +8,10 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 
 import tv.bain.bainsocial.R;
 import tv.bain.bainsocial.adapters.PostsAdapter;
@@ -29,16 +31,25 @@ public class HomeFrag extends Fragment {
     }
 
     private View initiateDataBinder(ViewGroup container) {
-        System.out.println("INITIATED");
         b = HomeFragmentBinding.inflate(getLayoutInflater(), container, false);
         return b.getRoot();
     }
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         vm = new ViewModelProvider(this).get(HomeViewModel.class);
         bindData();
+        initiateDrawer();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        /* Manages the shape of th Drawer's Hamburger button when the configuration changes*/
+        toggle.onConfigurationChanged(newConfig);
     }
 
     private void bindData() {
@@ -46,7 +57,15 @@ public class HomeFrag extends Fragment {
         b.setLifecycleOwner(getViewLifecycleOwner());
         b.setFrag(this);
         b.recycler.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+    }
+
+    private void initiateDrawer() {
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(b.toolbar);
+        toggle = new ActionBarDrawerToggle(requireActivity(), b.drawerLayout, b.toolbar, R.string.app_name, R.string.app_name);
+        b.drawerLayout.addDrawerListener(toggle);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        toggle.syncState();
     }
 
     @Override
@@ -61,7 +80,6 @@ public class HomeFrag extends Fragment {
 
 
     public void goToNewPostFrag() {
-        NavHostFragment.findNavController(this).navigate(R.id.action_homeFrag_to_postCreateFrag);
     }
 
 }
